c384cc76b6f4f533ccd2790c0857636aaaa691e3
denhuii <873023670@qq.com>
2023-04-21 01:57:10 +0800

    分类的编辑

--- models/Category.js
+++ models/Category.js
@@ -1,10 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const schema = new mongoose.Schema({
   name: {
     type: String,
   },
+  parent: {
+    type: mongoose.SchemaTypes.ObjectId,
+    ref: "Category",
+  }, // 关联自己 一对多   一级分类下面有多个二级分类  二级分类下面有多个三级分类  三级分类下面有多个四级分类  以此类推
 });
 
-
-module.exports = mongoose.model('Category', schema);
+module.exports = mongoose.model("Category", schema);
--- routes/admin/index.js
+++ routes/admin/index.js
@@ -6,25 +6,26 @@ module.exports = app => {
   const Category = require('../../models/Category');
 
   router.post('/categories', async (req, res) => {
-    console.log('🚀 ~ add categories', req.body);
     const model = await Category.create(req.body);
     res.send(model);
   });
 
-  router.get('/categories', async (req, res) => {
-    console.log('🚀 ~ get categories', req);
-    const items = await Category.find().limit(10);
+  router.put('/categories/:id', async (req, res) => {
+    const model = await Category.findByIdAndUpdate(req.params.id, req.body);
+    res.send(model);
+  });
+
+  router.get('/categories', async (req, res) => {  
+    const items = await Category.find().populate('parent').limit(10);
     res.send(items);
   });
 
   router.get('/categories/:id', async (req, res) => {
-    console.log('🚀 ~ get categories', req.params.id);
     const model = await Category.findById(req.params.id);
     res.send(model);
   });
 
   router.delete('/categories/:id', async (req, res) => {
-    console.log('🚀 ~ delete categories', req.params.id);
     await Category.findByIdAndRemove(req.params.id);
     res.send({ success: true });
   });
